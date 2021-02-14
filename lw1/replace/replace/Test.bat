73a2f3396ea31226e0542877c7b363173ffc44f2
MUKIVA <mukiva2261@gmail.com>
2021-02-14 20:21:01 +0300

    Добавлена 1 лаба

---
+++ lw1/replace/replace/Test.bat
@@ -0,0 +1,64 @@
+SET MyProgram="%~1"
+
+if %MyProgram%=="" (
+	echo Please specify path to program
+	exit /b 1
+)
+
+REM Test 1 : Проверка простой замены
+%MyProgram% InTest1.txt "%TEMP%\output.txt" "pog" "god" > nul || goto err
+fc "%TEMP%\output.txt" OutTest1.txt > nul || goto err
+echo Test 1 : successful
+REM Test 2 : Проверка замены "pog" на "pogpog"
+%MyProgram% InTest2.txt "%TEMP%\output.txt" "pog" "pogpog" > nul || goto err
+fc "%TEMP%\output.txt" OutTest2.txt > nul || goto err
+echo Test 2 : successful
+REM Test 3 : Проверка замены "pogpog" на "pog"
+%MyProgram% InTest3.txt "%TEMP%\output.txt" "pogpog" "pog" > nul || goto err
+fc "%TEMP%\output.txt" OutTest3.txt > nul || goto err
+echo Test 3 : successful
+REM Test 4 : Проверка замены "1231234" на "u"
+%MyProgram% InTest4.txt "%TEMP%\output.txt" "1231234" "u" > nul || goto err
+fc "%TEMP%\output.txt" OutTest4.txt > nul || goto err
+echo Test 4 : successful
+REM Test 5 : Проверка на пустые аргументы
+%MyProgram% InTest5.txt "%TEMP%\TheFileShouldNotExist.txt" "" "" > nul && goto err
+echo Test 5 : successful
+REM test 6 : Замена на самого себя
+%MyProgram% InTest6.txt "%TEMP%\output.txt" "pog" "pog" > nul || goto err
+fc "%TEMP%\output.txt" OutTest6.txt > nul || goto err
+echo Test 6 : successful
+REM test 7 : Проверка работы с пустым input файлом
+%MyProgram% InTest7.txt "%TEMP%\output.txt" "pog" "god" > nul || goto err
+fc "%TEMP%\output.txt" OutTest7.txt > nul || goto err
+echo Test 7 : successful
+REM test 8 : Проверка работы с несколькими строками
+%MyProgram% InTest8.txt "%TEMP%\output.txt" "pog" "god" > nul || goto err
+fc "%TEMP%\output.txt" OutTest8.txt > nul || goto err
+echo Test 8 : successful
+REM test 9 : Тест при отсутствии входного файла
+%MyProgram% "%TEMP%\TheFileShouldNotExist.txt" "pog" "god" > nul && goto err
+echo Test 9 : successful
+REM test 10 : Тест на отсутсвие выходного файла
+%MyProgram% Input.txt  "pog" "god" > nul && goto err
+echo Test 10 : successful
+REM test 11 : Тест на отсутсвие искомой подстроки
+%MyProgram% input.txt "%TEMP%\TheFileShouldNotExist.txt" "god" > nul && goto err
+echo Test 11 : successful
+REM test 12 : Тест на отсутсвие замены
+%MyProgram% input.txt "%TEMP%\TheFileShouldNotExist.txt" "pog" > nul && goto err
+echo Test 12 : successful
+REM test 13 : Тест на пустой input
+%MyProgram% "" "%TEMP%\TheFileShouldNotExist.txt" "pog" "god"> nul && goto err
+echo Test 13 : successful
+REM test 14 : Тест на путой out
+%MyProgram% input.txt "" "pog" "god" > nul && goto err
+echo Test 14 : successful
+REM Тесты завершились успешно
+echo All tests successful
+exit /B 0
+
+REM Один из тестов не прошел
+:err
+echo Test failed
+exit /B 1
---
+++ lw1/replace/replace/replace.cpp
@@ -0,0 +1,95 @@
+﻿#include <iostream>
+#include <string>
+#include <optional>
+#include <fstream>
+
+struct Args
+{
+    std::string inputFileName;
+    std::string outputFileName;
+    std::string searchString;
+    std::string replaceString;
+};
+
+std::optional<Args> ParseArgs(int argc, char* argv[])
+{
+    if (argc != 5)
+    {
+        std::cout << "Invalid argumets count\n";
+        std::cout << "Please use: replace.exe <input file> <output file> <search string> <replace string>\n";
+        return std::nullopt;
+    }
+    Args args;
+    args.inputFileName = argv[1];
+    args.outputFileName = argv[2];
+    args.searchString = argv[3];
+    args.replaceString = argv[4];
+    return args;
+}
+
+int main(int argc, char* argv[])
+{
+    auto args = ParseArgs(argc, argv);
+    if (!args) return 1;
+
+    if (args->searchString.length() == 0)
+    {
+        std::cout << "Search string argument is not defined\n";
+        return 1;
+    }
+    
+    // Open file for read
+    std::ifstream input;
+    input.open(args->inputFileName);
+    if (!input.is_open())
+    {
+        std::cout << "Failed to open '" << args->inputFileName << "'for reading\n";
+        return 1;
+    }
+    // Open file for write
+    std::ofstream output;
+    output.open(args->outputFileName);
+    if (!output.is_open())
+    {
+        std::cout << "Failed to open '" << args->inputFileName << "'for writing\n";
+        return 1;
+    }
+    // Get string from input file
+    std::string fileContent;
+    while (getline(input, fileContent))
+    {
+        //replace
+        int num = fileContent.find(args->searchString);
+        std::string result = "";
+        while (num != -1)
+        {
+            result.append(fileContent.substr(0, num + args->searchString.length()));
+            if (num != -1)
+            {
+                fileContent = fileContent.substr(num + args->searchString.length(), -1);
+            }
+            result.replace(result.rfind(args->searchString), args->searchString.length(), args->replaceString);
+            num = fileContent.find(args->searchString);
+        }
+        result.append(fileContent);
+        //Copy replaced string to output file
+        for (int i = 0; i < result.length(); i++)
+            output.put(result[i]);
+        output.put('\n');
+    }
+    
+    if (!output.flush())
+    {
+        std::cout << "failed to write data to output file";
+        return 1;
+    }
+
+    if (input.bad())
+    {
+        std::cout << "failed to read data from input file";
+        return 1;
+    }
+
+    std::cout << "Program completed successfully";
+    return 0;
+}

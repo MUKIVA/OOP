726abebd5f236589d0c20055252760effe3fb8db
MUKIVA <mukiva2261@gmail.com>
2021-02-21 20:56:47 +0300

    Задание 2 Radix

---
+++ lw1/radix/radix/radix.cpp
@@ -0,0 +1,122 @@
+﻿#include <iostream>
+#include <string>
+#include <optional>
+
+const int minRadix = 2;
+const int maxRadix = 36;
+
+
+struct Args
+{
+    std::string sourceNotation;
+    std::string destinationNotation;
+    std::string value;
+};
+
+std::optional<Args> ParseArgs(int argc, char* argv[])
+{
+    if (argc != 4)
+    {
+        std::cout << "Ivalid arguments count\n";
+        std::cout << "Please use: radix.exe <source notation> <destination notation> <value>\n";
+        return std::nullopt;
+    }
+    Args args;
+    args.sourceNotation = argv[1];
+    args.destinationNotation = argv[2];
+    args.value = argv[3];
+    return args;
+}
+// Функция перевода из строки в число
+int StringToInt(const std::string& str, int radix, bool& wasError)
+{
+    try
+    {
+        std::stoi(str, 0, radix);
+    }
+    catch (const std::exception& ex)
+    {
+        std::string what = ex.what();
+        if (what == "stoi argument out of range")
+        {
+            std::cout << "Value out of range\n";
+        }
+        if (what == "invalid stoi argument")
+        {
+            std::cout << "Incorrect arguments\n";
+        }
+        wasError = true;
+        return 1;
+    }
+    return std::stoi(str, 0, radix);
+}
+
+// Функция перевода из числа в строку
+std::string IntToString(int n, int radix, bool& wasError)
+{
+    const std::string digists = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    const int digitsLength = digists.length() - 1;
+    std::string result = "";
+
+    if (n < 0)
+    {
+        n *= -1;
+        result = "-";
+    }
+    // В случае если чило 0
+    if (n == 0)
+    {
+        result = '0';
+    }
+    // Перевод чилсла
+    while (n)
+    {
+        int curDigit = n % radix;
+        if (curDigit < 0 || curDigit > n % radix)
+        {
+            std::cout << "Error while converting a number to a string";
+            wasError = true;
+            return "";
+        }
+        result = digists[curDigit] + result;
+        n = n / radix;
+    }
+    if (result[result.length() - 1] == '-')
+    {
+        result = "-" + result.substr(0, result.find("-"));
+    }
+    return result;
+}
+
+int main(int argc, char* argv[])
+{
+    bool err = false;
+    auto args = ParseArgs(argc, argv);
+    
+    // Проверка диапазона оснований
+    int radix[2];
+    radix[0] = StringToInt(args->sourceNotation, 10, err);
+    if (err) return 1;
+    radix[1] = StringToInt(args->destinationNotation, 10, err);
+    if (err) return 1;
+    for (int num : radix)
+    {   
+        if (num > maxRadix && num < minRadix)
+        {
+            std::cout << "One of the notations out of range" << std::endl;
+            std::cout << "Please specify notation in the range from 2 to 36" << std::endl;
+            err = true;
+        }
+    }
+
+    // Перевод значения из числа в строку с конечным основанием
+    
+    int value = StringToInt(args->value, radix[0], err);
+    if (err) return 1;
+    std::string outStr = IntToString(value, radix[1], err);
+    if (err) return 1;
+
+    std::cout << outStr << std::endl;
+    // Программа оказалась успешной
+    return 0;
+}
---
+++ lw1/radix/radix/tests.bat
@@ -0,0 +1,52 @@
+SET MyProgram="%~1"
+
+REM Test 1 : Из меньшего основания в большее
+%MyProgram% "10" "16" "15" > "%TEMP%\out.txt" || goto err
+echo F> "output.txt"
+fc "%TEMP%\out.txt" "output.txt" > nul || goto err
+echo Test 1 passed
+REM Test 2 : Из большего основания в меньшее
+%MyProgram% "16" "10" "F" > "%TEMP%\out.txt" || goto err
+echo 15> "output.txt"
+fc "%TEMP%\out.txt" "output.txt" > nul || goto err
+echo Test 2 passed
+REM Test 3 : Третий аргумент пустой
+%MyProgram% "16" "10" "" > "%TEMP%\out.txt" && goto err
+echo Test 3 passed
+REM Test 4 : Второй аргумент пустой
+%MyProgram% "16" "" "F" > "%TEMP%\out.txt" && goto err
+echo Test 4 passed
+REM Test 5 : Первый аргумент пустой
+%MyProgram% "" "10" "F" > "%TEMP%\out.txt" && goto err
+echo Test 5 passed
+REM Test 6 : Аргументов слишком много
+%MyProgram% "16" "10" "F" "100" > "%TEMP%\out.txt" && goto err
+echo Test 6 passed
+REM Test 7 : Аргументов слишком мало
+%MyProgram% "16" "10" > "%TEMP%\out.txt" && goto err
+echo Test 7 passed
+REM Test 8 : Проверка перевода цифры 0
+%MyProgram% "16" "10" "0" > "%TEMP%\out.txt" || goto err
+fc "%TEMP%\out.txt" "zero.txt" > nul || goto err
+echo Test 8 passed
+REM Test 9 : Перевод отрицательного числа
+%MyProgram% "16" "10" "-F" > "%TEMP%\out.txt" || goto err
+echo -15> "output.txt"
+fc "%TEMP%\out.txt" "output.txt" > nul || goto err
+echo Test 9 passed
+REM Test 10 : Если число оказалось вне границ
+%MyProgram% "10" "16" "2147483648" > "%TEMP%\out.txt" && goto err
+echo Test 10 passed
+REM Test 11 : проверка крайнего значения
+%MyProgram% "10" "16" "2147483647" > "%TEMP%\out.txt" || goto err
+echo 7FFFFFFF> "output.txt"
+fc "%TEMP%\out.txt" "output.txt" > nul || goto err
+echo Test 11 passed
+REM Тесты прошли успешно
+exit /B 0
+
+
+REM В случае воникновения ошибок
+:err
+echo Test failed
+exit /B 1
